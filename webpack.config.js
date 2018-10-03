3a2a3910db8d89286bb1c06b13caf9e07b4414dd
Dmitry Volovod <dvolovod@it.ru>
2018-10-03 17:06:47 +0300

    add project setup

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname,'src/js/app.js'),
+    style: path.join(__dirname, './src/css/style.css')
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use:  ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new MiniCssExtractPlugin({
+      filename: 'style.[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/html/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+    new CopyWebpackPlugin([
+      { from: './src/assets', to: './assets'}
+    ]),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
